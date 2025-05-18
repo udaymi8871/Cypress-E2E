0bff0f7047a11bf6f733d45fa7ba444c857e2cc9
apple <apple@apples-MacBook-Pro.local>
2025-05-18 16:34:56 +0530

    test parallel

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,15 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
+
+reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     baseUrl: 'https://naveenautomationlabs.com/opencart',
     setupNodeEvents(on, config) {
       return config;
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
     env: {
       loginPath: '/index.php?route=account/login',
