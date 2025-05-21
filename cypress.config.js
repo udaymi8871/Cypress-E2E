8b40973004dbd99d285c06a9d126c403c909bd9d
apple <apple@apples-MacBook-Pro.local>
2025-05-21 15:28:31 +0530

    report file

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,48 @@
-// cypress.config.js
+// // cypress.config.js
+// const { defineConfig } = require("cypress");
+// require("dotenv").config();
+
+// module.exports = defineConfig({
+
+// reporter: 'cypress-mochawesome-reporter',
+
+//   e2e: {
+//     baseUrl: 'https://naveenautomationlabs.com/opencart',
+//     setupNodeEvents(on, config) {
+//       return config;
+//       require('cypress-mochawesome-reporter/plugin')(on);
+
+//     },
+//     env: {
+//       loginPath: '/index.php?route=account/login',
+//       registerPath: '/index.php?route=account/register'
+//     }
+//   }
+// });
+
+
+
 const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
-
-reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html',
+    overwrite: false,
+    html: true,
+    json: true,
+    timestamp: 'mmddyyyy_HHMMss'
+  },
 
   e2e: {
     baseUrl: 'https://naveenautomationlabs.com/opencart',
-    setupNodeEvents(on, config) {
-      return config;
-      require('cypress-mochawesome-reporter/plugin')(on);
 
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on); // ✅ This now gets executed
+      return config;
     },
+
     env: {
       loginPath: '/index.php?route=account/login',
       registerPath: '/index.php?route=account/register'
